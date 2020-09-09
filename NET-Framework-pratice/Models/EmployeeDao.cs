2a8e445432d9bdb3292d14005915785d2bb58cbe
gn02618594 <gn02618594@gmail.com>
2020-09-09 22:46:21 +0800

    feat: 本來是在外面直接加 Employee 類別,但後來發現 應該是要加在 Models 裡,並且是分成 Employee 跟 EmployeeDao 兩個 class

---
+++ NET-Framework-pratice/Models/EmployeeDao.cs
@@ -0,0 +1,17 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+
+namespace NET_Framework_pratice.Models
+{
+    public class EmployeeDao
+    {
+        public static List<Employee> Employees { get; set; } = new List<Employee>
+        {
+            new Employee {EmployeeId=1,Name="Anson",Address="台北",PhoneNumber="0911" },
+            new Employee {EmployeeId=2,Name="Jason",Address="高雄",PhoneNumber="0922" },
+            new Employee {EmployeeId=3,Name="Andy",Address="新竹",PhoneNumber="0910" },
+        };
+    }
+}
